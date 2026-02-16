58917f8a78f8c36457c720bffc73bbb838bc51ec
Matthew Frost <Exupery@users.noreply.github.com>
2026-02-16 16:41:18 -0500

    updated for midnight

--- DontCast.lua
+++ DontCast.lua
@@ -11,20 +11,12 @@ local playerServer = nil
 local config = {}
 local tempConfig = {} -- used to copy profile settings
 local profiles = {}
-local auras = {}
 
 local upAuras = {}
-local updCtr = 0
 
 local DEFAULT_POINT = "CENTER"
 local DEFAULT_WIDTH = 300
 local DEFAULT_HEIGHT = 50
-
-local BASE = "base"
-local MAGICAL = "magical"
-local PHYSICAL = "physical"
-
-local MAX_AURAS = 40
 
 local SOUNDS = {
   None = "",
@@ -112,7 +104,6 @@ end
 local function resized(frame, width, height)
   local font = textFrame:GetFont()
   iconFrame:SetSize(height, height)
-  cdTextFrame:SetFont(font, height * 0.5)
   textFrame:SetFont(font, height * 0.7)
   textFrame:SetPoint("LEFT", height * 1.1, 0)
 end
@@ -155,7 +146,6 @@ end
 
 local function defaultConfig()
   return {
-    threshold = 1.5,
     fontstyle = "Arial",
     fontalignment = "Left",
     aurabeginsound = "",
@@ -220,7 +210,6 @@ end
 local function setFontStyle(style)
   local font = FONTS[style]
   if not font then return end
-  cdTextFrame:SetFont(font, mainFrame:GetHeight() * 0.5)
   textFrame:SetFont(font, mainFrame:GetHeight() * 0.7)
   updateConfig("fontstyle", style)
   movingOrSizingStopped()
@@ -234,261 +223,15 @@ local function setFontAlignment(justification)
   movingOrSizingStopped()
 end
 
-local function addAurasToList(list, values)
-  for _, id in ipairs(values) do
-    local spellInfo = C_Spell.GetSpellInfo(id)
-    if spellInfo then
-      local name = spellInfo["name"]
-      list[name] = true
-    end
-  end
-end
-
-local function defaultBaseAuras()
-  return {
-    186265, -- Aspect of the Turtle
-    409293, -- Burrow
-    31230,  -- Cheat Death
-    33786,  -- Cyclone
-    108416, -- Dark Pact
-    19263,  -- Deterrence
-    47585,  -- Dispersion
-    642,    -- Divine Shield
-    228049, -- Guardian of the Forgotten Queen
-    45438,  -- Ice Block
-    221527, -- Imprison
-    116849, -- Life Cocoon
-    196555, -- Netherwalk
-    115078, -- Paralysis
-    28272,  -- Polymorph
-    200851, -- Rage of the Sleeper
-    184662, -- Shield of Vengeance
-    76577,  -- Smoke Bomb
-    263648, -- Soul Barrier
-    198111, -- Temporal Shield
-    378441, -- Time Stop
-    219809, -- Tombstone
-    122470, -- Touch of Karma
-  }
-end
-
-local function defaultMagicalAuras()
-  return {
-    48707,  -- Anti-Magic Shell
-    204018, -- Blessing of Spellwarding
-    31224,  -- Cloak of Shadows
-    122783, -- Diffuse Magic
-    8178,   -- Grounding Totem
-    212295, -- Nether Ward
-    23920,  -- Spell Reflection
-  }
-end
-
-local function defaultPhysicalAuras()
-  return {
-    1022,   -- Blessing of Protection
-    198589, -- Blur
-    118038, -- Die by the Sword
-    205191, -- Eye for an Eye
-    5277,   -- Evasion
-    199754, -- Riposte
-    305497, -- Thorns
-  }
-end
-
-local function defaultAuras()
-  local defaults = {}
-  defaults[BASE] = defaultBaseAuras()
-  defaults[MAGICAL] = defaultMagicalAuras()
-  defaults[PHYSICAL] = defaultPhysicalAuras()
-  return defaults
-end
-
-local function isMagical()
-  -- return early if spec not set yet (i.e. addon may get loaded before GetSpecialization returns non-nil value)
-  -- defaulting to true since addon was originally written for casters but this shouldn't
-  -- end up mattering as the auras assignment will get re-called when ACTIVE_TALENT_GROUP_CHANGED fires
-  if GetSpecialization() == nil then return true end
-
-  local casterIds = {[62] = true, [63] = true, [64] = true, [102] = true, [258] = true, [262] = true, [265] = true, [266] = true, [267] = true, [1467] = true}
-
-  return casterIds[GetSpecializationInfo(GetSpecialization())]
-end
-
-local function isHealer()
-  if GetSpecialization() == nil then return false end
-
-  local healerIds = {[65] = true, [105] = true, [256] = true, [257] = true, [264] = true, [270] = true, [1468] = true}
-  return healerIds[GetSpecializationInfo(GetSpecialization())]
-end
-
--- adds values from secondTable to firstTable
-local function mergeTables(firstTable, secondTable)
-  for k, v in pairs(secondTable) do firstTable[k] = v end
-end
-
-local function savedAuras()
-  if not DontCastAuras then
-    DontCastAuras = {}
-  end
-
-  if DontCastAuras[BASE] == nil then
-    DontCastAuras[BASE] = {}
-    -- migrate pre-1.4 auras to handle user-added auras
-    for k, v in pairs(DontCastAuras) do
-      if string.find(k, BASE) == nil then
-        DontCastAuras[BASE][k] = v
-      end
-    end
-    for k, _ in pairs(DontCastAuras[BASE]) do
-      DontCastAuras[k] = nil
-    end
-    addAurasToList(DontCastAuras[BASE], defaultBaseAuras())
-  end
-
-  if DontCastAuras[MAGICAL] == nil then
-    DontCastAuras[MAGICAL] = {}
-    addAurasToList(DontCastAuras[MAGICAL], defaultMagicalAuras())
-  end
-
-  if DontCastAuras[PHYSICAL] == nil then
-    DontCastAuras[PHYSICAL] = {}
-    addAurasToList(DontCastAuras[PHYSICAL], defaultPhysicalAuras())
-  end
-
-  local specAuras = {}
-  mergeTables(specAuras, DontCastAuras[BASE])
-  if isMagical() then
-    mergeTables(specAuras, DontCastAuras[MAGICAL])
-  elseif isHealer() then
-    mergeTables(specAuras, DontCastAuras[MAGICAL])
-    mergeTables(specAuras, DontCastAuras[PHYSICAL])
-  else
-    mergeTables(specAuras, DontCastAuras[PHYSICAL])
-  end
-  return specAuras
-end
-
-local function addAura(aura, listName)
-  if aura == nil then return end
-  DontCastAuras[listName][aura] = true
-  if DontCastAuras[listName][aura] then
-    auras = savedAuras()
-    colorPrint("Added "..aura)
-  else
-    errorPrint("Unable to add "..aura)
-  end
-end
-
-local function removeAura(aura)
-  for listName, _ in pairs(defaultAuras()) do
-    if DontCastAuras[listName][aura] then
-      DontCastAuras[listName][aura] = false
-      if not DontCastAuras[listName][aura] then
-        auras = savedAuras()
-        colorPrint("Removed "..aura)
-      else
-        errorPrint("Unable to remove "..aura)
-      end
-    end
-  end
-end
-
-local function addNewDefaults()
-  for listName, list in pairs(defaultAuras()) do
-    for _, auraId in pairs(list) do
-      local spellInfo = C_Spell.GetSpellInfo(auraId)
-      if not spellInfo then
-        errorPrint("Invalid AuraID "..auraId)
-      elseif DontCastAuras[listName][spellInfo["name"]] == nil then
-        addAura(spellInfo["name"], listName)
-      end
-    end
-  end
-end
-
-local function displayAuras()
-  colorPrint("DontCast is triggered by the following:")
-  for aura, _ in pairs(auras) do
-    if auras[aura] then print(aura) end
-  end
-end
-
-local function formatTime(remaining)
-  if remaining < 3 then
-    return string.format("%.1f", remaining)
-  elseif remaining >= 3 and remaining < 100 then
-    return string.format("%.0f", remaining)
-  else
-    return string.format("%.0f", remaining / 60).."m"
-  end
-end
-
-local function displayCountdown(duration)
-  local txt = ""
-  if duration and duration > 0 then
-    if duration < config.threshold then
-      cdTextFrame:SetTextColor(1, 0.1, 0.1, 1)
-    else
-      cdTextFrame:SetTextColor(1, 1, 0.1, 0.85)
-    end
-    txt = formatTime(duration)
-  end
-  cdTextFrame:SetText(txt)
-end
-
-local function unitInSmoke(unit, localalizedSmokeBomb)
-  for i = 1, MAX_AURAS do
-    local spellInfo = C_UnitAuras.GetAuraDataByIndex(unit, i, "HARMFUL")
-    if spellInfo then
-      local name = spellInfo["name"]
-      if name == localalizedSmokeBomb then
-        return true
-      end
-    end
-  end
-  return false
-end
-
-local function isValid(name)
-  if auras[name] == nil or auras[name] == false then
-    return false
-  end
-
-  local localalizedSmokeBomb = C_Spell.GetSpellInfo(76577)["name"]
-  local localalizedTouchOfKarma = C_Spell.GetSpellInfo(122470)["name"]
-  local localalizedThorns = C_Spell.GetSpellInfo(305497)["name"]
-  if (name == localalizedSmokeBomb) then
-    --only concerned with Smoke Bomb when player NOT also in smoke
-    local targetInSmoke = unitInSmoke("target", localalizedSmokeBomb)
-    local playerInSmoke = unitInSmoke("player", localalizedSmokeBomb)
-    if (targetInSmoke and playerInSmoke) then
-      return false
-    end
-    return true
-  elseif (name == localalizedTouchOfKarma) then
-    --only display ToK if target has the buff, i.e. not recipients of the ToK dot debuff
-    for i = 1, MAX_AURAS do
-      local buffInfo = C_UnitAuras.GetBuffDataByIndex("target", i)
-      if buffInfo then
-        local buffName = buffInfo["name"]
-        if buffName == localalizedTouchOfKarma then return true end
-      end
-    end
-    return false
-  elseif (name == localalizedThorns) then
-    --only display the (damage) buff, not the (slow) debuff
-    for i = 1, MAX_AURAS do
-      local buffInfo = C_UnitAuras.GetBuffDataByIndex("target", i)
-      if buffInfo then
-        local buffName = buffInfo["name"]
-        if buffName == localalizedThorns then return true end
-      end
-    end
-    return false
-  end
-
-  return true
+local function getMajorDefensives(unit)
+  local result = {}
+  AuraUtil.ForEachAura(unit, "HELPFUL|BIG_DEFENSIVE", nil, function(auraData)
+    table.insert(result, auraData)
+  end, true)
+  AuraUtil.ForEachAura(unit, "HELPFUL|EXTERNAL_DEFENSIVE", nil, function(auraData)
+    table.insert(result, auraData)
+  end, true)
+  return result
 end
 
 local function targetIsHostile()
@@ -496,7 +239,7 @@ local function targetIsHostile()
 end
 
 local function setAura(name, icon)
-  if name and isValid(name) then
+  if name then
     textFrame:SetText(name)
     iconFrame:SetTexture(icon)
     if not mainFrame:IsShown() and SOUNDS[config.aurabeginsound] ~= nil then
@@ -509,28 +252,13 @@ local function setAura(name, icon)
   return false
 end
 
-local function setAuras(unit, filter)
-  local hasAura = false
-  for i = 1, MAX_AURAS do
-    local spellInfo = C_UnitAuras.GetAuraDataByIndex(unit, i, filter)
-    if spellInfo then
-      local name = spellInfo["name"]
-      local icon = spellInfo["icon"]
-      if not name then break end
-      if not hasAura then
-        hasAura = setAura(name, icon)
-      end
-      if hasAura then break end
-    end
-  end
-  return hasAura
-end
-
 local function auraUpdated(self, event, unit, ...)
   if unit == "target" and targetIsHostile() then
-    local hasAura = setAuras(unit, "HELPFUL")
-    if not hasAura then
-      hasAura = setAuras(unit, "HARMFUL")
+    local defensives = getMajorDefensives(unit)
+    local hasAura = false
+    if #defensives > 0 then
+      local first = defensives[1]
+      hasAura = setAura(first.name, first.icon)
     end
 
     if not hasAura then
@@ -554,33 +282,6 @@ local function targetChanged(self, event, unit, ...)
   end
 end
 
-local function updateAuraTime(aura, unit, filter)
-  for i = 1, MAX_AURAS do
-    local spellInfo = C_UnitAuras.GetAuraDataByIndex(unit, i, filter)
-    if spellInfo then
-      local name = spellInfo["name"]
-      local expTime = spellInfo["expirationTime"]
-      if name == aura and expTime ~= nil then
-        displayCountdown(expTime - GetTime())
-      end
-    end
-  end
-end
-
-local function onUpdate(self, elapsed)
-  updCtr = updCtr + elapsed
-  if updCtr > 0.1 and mainFrame:IsShown() then
-    local aura = textFrame:GetText()
-    if aura then
-      for i = 1, MAX_AURAS do
-        updateAuraTime(aura, "target", "HELPFUL")
-        updateAuraTime(aura, "target", "HARMFUL")
-      end
-    end
-    updCtr = 0
-  end
-end
-
 local function fontStyleSelected(self)
   setFontStyle(self.value)
 end
@@ -596,21 +297,6 @@ local function createButton(text, parent)
   button:SetText(text)
   button:ClearAllPoints()
   return button
-end
-
-local function setInputBoxText(inputBox, text)
-  inputBox:SetText(text)
-  inputBox:SetCursorPosition(0)
-end
-
-local function createInputBox(name, parent, defaultText)
-  local box = CreateFrame("EditBox", "DontCast"..name.."EditBox", parent, "InputBoxTemplate")
-  box:SetHeight(20)
-  box:SetWidth(35)
-  box:SetAutoFocus(false)
-  box:ClearAllPoints()
-  setInputBoxText(box, defaultText)
-  return box
 end
 
 local function sortedKeys(tbl)
@@ -663,32 +349,6 @@ local function drawPositioningOptions(parent, xOffset, yOffset)
 
   centerButton:SetPoint("TOPLEFT", xOffset * 3 + buttonWidth * 2, yOffset)
   centerButton:SetScript("PostClick", centerFrame)
-end
-
-local function setThreshold(threshold, echo)
-  local asNum = tonumber(threshold)
-  if type(asNum) == "number" then
-    local updated = updateConfig("threshold", asNum)
-    if updated then
-      if echo then colorPrint("Threshold set to "..threshold) end
-      setInputBoxText(optionsFrame.threshold, threshold)
-    else
-      errorPrint("Unable to set threshold to "..threshold)
-    end
-  elseif echo then
-    errorPrint("Threshold NOT changed! Must be set to a number.")
-  end
-end
-
-local function drawThresholdOptions(parent, xOffset, yOffset)
-  local label = createLabel("Expiring soon threshold (seconds)", parent, xOffset, yOffset)
-
-  parent.threshold = createInputBox("threshold", parent, config.threshold)
-  parent.threshold:SetMaxLetters(4)
-  parent.threshold:SetPoint("LEFT", label, "RIGHT", 10, 0)
-  parent.threshold:SetScript("OnTextChanged", function(self, userInput)
-    setThreshold(optionsFrame.threshold:GetText(), false)
-  end)
 end
 
 local function getSelectedFontStyle()
@@ -762,7 +422,6 @@ local function copyConfigSelected(self)
     tempConfig[k] = v
     config[k] = v
   end
-  setInputBoxText(optionsFrame.threshold, tempConfig.threshold)
   setFontStyle(tempConfig.fontstyle)
   setFontAlignment(tempConfig.fontalignment)
   if tempConfig.point ~= nil then
@@ -791,7 +450,6 @@ local function drawCopyConfigOptions(parent, xOffset, yOffset)
 end
 
 local function updateOptionsUI()
-  setInputBoxText(optionsFrame.threshold, config.threshold)
   reloadDropDowns()
 end
 
@@ -811,10 +469,9 @@ local function createOptionsPanel()
   profiles = copyConfigEntries()
 
   drawPositioningOptions(optionsFrame, xOffset, -50)
-  drawThresholdOptions(optionsFrame, xOffset, -90)
-  drawFontStyleOptions(optionsFrame, xOffset, -130)
-  drawSoundOptions(optionsFrame, xOffset, -170)
-  drawCopyConfigOptions(optionsFrame, xOffset, -270)
+  drawFontStyleOptions(optionsFrame, xOffset, -90)
+  drawSoundOptions(optionsFrame, xOffset, -130)
+  drawCopyConfigOptions(optionsFrame, xOffset, -230)
 
   updateOptionsUI()
 end
@@ -844,12 +501,8 @@ local function eventHandler(self, event, unit, ...)
     targetChanged(self, event, unit)
   elseif event == "PLAYER_REGEN_DISABLED" then
     lockFrame(false)
-  elseif event == "ACTIVE_TALENT_GROUP_CHANGED" or event == "PLAYER_ENTERING_WORLD" then
-    auras = savedAuras()
   elseif event == "ADDON_LOADED" and unit == "DontCast" then
     playerServer = UnitName("player").." - "..GetRealmName()
-    auras = savedAuras()
-    addNewDefaults()
     config = savedConfig()
     mainFrame:SetPoint(config.point, UIParent, config.relativePoint, config.xOfs, config.yOfs)
     updateSoundConfig()
@@ -920,11 +573,8 @@ function loadDontCast(self, text, icon, cdText)
     eventFrame:RegisterEvent("ADDON_LOADED")
     eventFrame:RegisterEvent("UNIT_AURA")
     eventFrame:RegisterEvent("PLAYER_REGEN_DISABLED")
-    eventFrame:RegisterEvent("ACTIVE_TALENT_GROUP_CHANGED")
-    eventFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
     eventFrame:RegisterEvent("ZONE_CHANGED_NEW_AREA")
     eventFrame:SetScript("OnEvent", eventHandler)
-    eventFrame:SetScript("OnUpdate", onUpdate)
 
     lockFrame(true)
     registerFonts()
@@ -942,52 +592,11 @@ SlashCmdList["DONTCAST"] = function(cmd)
       lockFrame(true)
     elseif cmd == "center" then
       centerFrame()
-    elseif string.match(cmd, "add%s+.+") then
-      local aura = string.match(cmd, "add%s+(.+)")
-      if aura then
-        addAura(aura, BASE)
-      end
-    elseif string.match(cmd, "addm%s+.+") then
-      local aura = string.match(cmd, "addm%s+(.+)")
-      if aura then
-        addAura(aura, MAGICAL)
-      end
-    elseif string.match(cmd, "addp%s+.+") then
-      local aura = string.match(cmd, "addp%s+(.+)")
-      if aura then
-        addAura(aura, PHYSICAL)
-      end
-    elseif string.match(cmd, "remove%s+.+") then
-      local aura = string.match(cmd, "remove%s+(.+)")
-      if aura then
-        removeAura(aura)
-      end
-    elseif string.match(cmd, "show%s+threshold") then
-      colorPrint("Countdown text changes color at "..config.threshold.." seconds")
-    elseif string.match(cmd, "threshold%s+[0-9.]+") then
-      local threshold = string.match(cmd, "threshold%s+(.+)")
-      if threshold then
-        setThreshold(threshold, true)
-      end
-    elseif cmd == "list" then
-      displayAuras()
-    elseif cmd == "default" then
-      DontCastAuras = defaultAuras()
-      auras = savedAuras()
-      colorPrint("DontCast reverted to default triggers")
     elseif string.match(cmd, "config%w*") then
       Settings.OpenToCategory("DontCast")
       updateOptionsUI() -- values may have been modified via slash commands
     else
       colorPrint("DontCast commands:")
-      print("/dontcast add NAME - adds the named buff or debuff")
-      print("/dontcast addm NAME - adds the named buff or debuff for magical mode only")
-      print("/dontcast addp NAME - adds the named buff or debuff for physical mode only")
-      print("/dontcast remove NAME - removes the named buff or debuff")
-      print("/dontcast threshold #.## - set the threshold for changing color of countdown text")
-      print("/dontcast show threshold - display the threshold color of countdown text changes")
-      print("/dontcast list - display what will trigger the warning")
-      print("/dontcast default - reverts to the default triggers")
       print("/dontcast show - Shows the addon for repositioning and resizing")
       print("/dontcast hide - Hides (and locks) the frame")
       print("/dontcast center - Sets the position to center of screen")
